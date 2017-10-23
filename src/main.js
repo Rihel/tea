1c7385ff9a6773dcc5e1be85af04c114349c8903
rihel <569843207@qq.com>
2017-10-24 00:43:37 +0800

    Add

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from './store'
+import VueResource from 'vue-resource';
 Vue.config.productionTip = false
-
+Vue.use(VueResource);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+	router,
+	store,
   template: '<App/>',
   components: { App }
 })
---
+++ src/pages/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		home
+		<div class="box">
+
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	
+}
+</script>
+<style lang="scss">
+@import "../../assets/scss/_mixin.scss";
+.box{
+	@include dpr(2){
+		@include bg2(brand);
+	}
+	@include dpr(3){
+		@include bg3(brand);
+	}
+	
+
+
+}
+</style>
+
